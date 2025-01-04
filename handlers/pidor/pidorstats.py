eecc0aed47edfd3d6737eded95c71096a744d84e
Aether <aether@pierdoling.org>
2025-01-04 23:03:59 +0300

    fix(pidorstats): don't notify people

--- handlers/pidor/pidorstats.py
+++ handlers/pidor/pidorstats.py
@@ -1,7 +1,7 @@
 import traceback
 
 from aiogram import html
-from aiogram.exceptions import TelegramBadRequest
+from aiogram.exceptions import TelegramBadRequest, TelegramForbiddenError
 from aiogram.types import Message
 
 from handlers.pidor import db
@@ -22,19 +22,13 @@ async def pidorstats(message: Message) -> None:
         try:
             member = await message.bot.get_chat_member(message.chat.id, user_id)
             if member.user.username is not None:
-                name = html.quote(member.user.username)
-                mention = f"{html.quote(name)}"
+                name = f"{html.quote(member.user.username)}"
             else:
                 name = html.quote(member.user.first_name)
-        except TelegramBadRequest:
+        except (TelegramForbiddenError, TelegramBadRequest):
             name = user_id  # In worst case, use the user_id
-        finally:
-            try:
-                mention
-            except NameError:  # User does not have a username
-                mention = f'<a href="tg://user?id={user_id}">{name}</a>'
 
-        return mention
+        return name
 
     text = "<b>Статистика пидоров за последний год:</b>\n"
     for entry in stats:
