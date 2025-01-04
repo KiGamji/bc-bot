fef07113e8a9d990279114563ce198d8f76ef729
Aether <aether@pierdoling.org>
2025-01-04 23:10:41 +0300

    feat(pidor): locks for chats
    so that no multiple pidors at once can be selected

--- handlers/pidor/pidor.py
+++ handlers/pidor/pidor.py
@@ -56,68 +56,78 @@ FINAL_STRINGS = [
     "Кто бы мог подумать, но <b>пидор дня</b> - "
 ]
 
+locks = {}
+
 
 async def pidor(message: Message) -> None:
-    check_result = await db.get_todays_pidor(message.chat.id)
-    if check_result:
-        pidor_id, last_timestamp = check_result
-        next_allowed_timestamp = last_timestamp + 86400
-        current_timestamp = int(datetime.datetime.now().timestamp())
-        time_left_seconds = next_allowed_timestamp - current_timestamp
-
-        if time_left_seconds > 0:
-            # Cooldown has not expired yet
-            time_left_delta = datetime.timedelta(seconds=time_left_seconds)
-            formatted_time_left = format_timedelta_ru(time_left_delta)
-
-            try:
-                member = await message.bot.get_chat_member(message.chat.id, pidor_id)
-                if member.user.username is not None:
-                    name = html.quote(member.user.username)
-                    mention = f"@{html.quote(name)}"
-                else:
-                    name = html.quote(member.user.first_name)
-            except TelegramBadRequest:
-                name = pidor_id  # In worst case, use the user_id
-            finally:
-                try:
-                    mention
-                except NameError:  # User does not have a username
-                    mention = f'<a href="tg://user?id={pidor_id}">{name}</a>'
-
-            await message.reply(
-                f"<b>Сегодняшний пидор</b> - {mention.replace('@', '')}\n"
-                f"Нового пидора можно будет выбрать через {formatted_time_left}"
-            )
-            return
-
-    pidors = await db.get_pidors(message.chat.id)
-    if not pidors:
-        await message.reply("<b>Никто не зарегистрирован в игре. Зарегистрироваться -</b> /pidoreg")
+    if message.chat.id in locks.keys():
         return
-    pidor_id = random.choice(pidors)
+    else:
+        locks[message.chat.id] = True
 
     try:
-        member = await message.bot.get_chat_member(message.chat.id, pidor_id)
-        if member.user.username is not None:
-            name = html.quote(member.user.username)
-            mention = f"@{html.quote(name)}"
-        else:
-            name = html.quote(member.user.first_name)
-    except TelegramBadRequest:
-        name = pidor_id  # In worst case, use the user_id
-    finally:
+        check_result = await db.get_todays_pidor(message.chat.id)
+        if check_result:
+            pidor_id, last_timestamp = check_result
+            next_allowed_timestamp = last_timestamp + 86400
+            current_timestamp = int(datetime.datetime.now().timestamp())
+            time_left_seconds = next_allowed_timestamp - current_timestamp
+
+            if time_left_seconds > 0:
+                # Cooldown has not expired yet
+                time_left_delta = datetime.timedelta(seconds=time_left_seconds)
+                formatted_time_left = format_timedelta_ru(time_left_delta)
+
+                try:
+                    member = await message.bot.get_chat_member(message.chat.id, pidor_id)
+                    if member.user.username is not None:
+                        name = html.quote(member.user.username)
+                        mention = f"@{html.quote(name)}"
+                    else:
+                        name = html.quote(member.user.first_name)
+                except TelegramBadRequest:
+                    name = pidor_id  # In worst case, use the user_id
+                finally:
+                    try:
+                        mention
+                    except NameError:  # User does not have a username
+                        mention = f'<a href="tg://user?id={pidor_id}">{name}</a>'
+
+                await message.reply(
+                    f"<b>Сегодняшний пидор</b> - {mention.replace('@', '')}\n"
+                    f"Нового пидора можно будет выбрать через {formatted_time_left}"
+                )
+                return
+
+        pidors = await db.get_pidors(message.chat.id)
+        if not pidors:
+            await message.reply("<b>Никто не зарегистрирован в игре. Зарегистрироваться -</b> /pidoreg")
+            return
+        pidor_id = random.choice(pidors)
+
         try:
-            mention
-        except NameError:  # User does not have a username
-            mention = f'<a href="tg://user?id={pidor_id}">{name}</a>'
+            member = await message.bot.get_chat_member(message.chat.id, pidor_id)
+            if member.user.username is not None:
+                name = html.quote(member.user.username)
+                mention = f"@{html.quote(name)}"
+            else:
+                name = html.quote(member.user.first_name)
+        except TelegramBadRequest:
+            name = pidor_id  # In worst case, use the user_id
+        finally:
+            try:
+                mention
+            except NameError:  # User does not have a username
+                mention = f'<a href="tg://user?id={pidor_id}">{name}</a>'
 
-    await message.answer(random.choice(BEGINNING_STRINGS))
-    await asyncio.sleep(3)
-    await message.answer(random.choice(PROCESS_STRINGS))
-    await asyncio.sleep(3)
-    await message.answer(random.choice(PROCESS_STRINGS))
-    await asyncio.sleep(3)
-    await message.answer(random.choice(FINAL_STRINGS) + mention)
+        await message.answer(random.choice(BEGINNING_STRINGS))
+        await asyncio.sleep(3)
+        await message.answer(random.choice(PROCESS_STRINGS))
+        await asyncio.sleep(3)
+        await message.answer(random.choice(PROCESS_STRINGS))
+        await asyncio.sleep(3)
+        await message.answer(random.choice(FINAL_STRINGS) + mention)
 
-    await db.mark_as_used_pidor(message.chat.id, pidor_id)
+        await db.mark_as_used_pidor(message.chat.id, pidor_id)
+    finally:
+        del locks[message.chat.id]
