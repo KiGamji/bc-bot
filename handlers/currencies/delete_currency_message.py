722b1b07f0001a1a714b5078795bfcb642594d30
KiGamji <maloyh44@gmail.com>
2025-08-09 00:56:40 +0500

    fix: properly get username via get_mention in delete_currency_message

--- handlers/currencies/delete_currency_message.py
+++ handlers/currencies/delete_currency_message.py
@@ -1,15 +1,19 @@
 from aiogram import html
 from aiogram.types import CallbackQuery
 
+from handlers.utils import get_mention
+
 
 async def delete_currency_message(call: CallbackQuery) -> None:
-    username = call.from_user.username
+    chat_id = call.message.chat.id
     user_id = call.from_user.id
+
+    username = await get_mention(chat_id, user_id, True)
 
     await call.message.edit_text(
         f"{call.message.text}"
         "\n\n"
-        f"{html.bold('– удалил(-а)')} @{html.quote(username)} ({html.code(user_id)})."
+        f"{html.bold('– удалил(-а)')} {username} ({html.code(user_id)})."
     )
 
     await call.message.delete()
